8f3af1e28edd0efd1d77c5fd32932f1b90fe70ea
Pallay Raunu <pallay@pixelclock.co.uk>
2012-08-15 21:24:08 +0100

    Adding step definition for movie creation

--- Gemfile
+++ Gemfile
@@ -8,12 +8,7 @@ gem 'rails', '3.1.0'
 # for Heroku deployment - as described in Ap. A of ELLS book
 group :development, :test do
   gem 'sqlite3'
-  gem 'ruby-debug19', :require => 'ruby-debug'
-  gem 'cucumber-rails'
-  gem 'cucumber-rails-training-wheels'
-  gem 'database_cleaner'
-  gem 'capybara'
-  gem 'launchy'
+  # gem 'ruby-debug19', :require => 'ruby-debug'
 end
 group :production do
 #  gem 'pg'
@@ -22,13 +17,21 @@ end
 # Gems used only for assets and not required
 # in production environments by default.
 group :assets do
-  gem 'therubyracer'              
+  gem 'therubyracer'
   gem 'sass-rails', "  ~> 3.1.0"
   gem 'coffee-rails', "~> 3.1.0"
   gem 'uglifier'
 end
 
 gem 'jquery-rails'
+
+group :test do
+  gem 'cucumber-rails', :require => false
+  gem 'cucumber-rails-training-wheels'
+  gem 'database_cleaner'
+  gem 'capybara'
+  gem 'launchy'
+end
 
 # Use unicorn as the web server
 # gem 'unicorn'
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,10 +4,18 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create({:title => 'Aladdin',         :rating => 'G',       :release_date => '25-Nov-1992'});
+    Movie.create({:title => 'The Terminator',  :rating => 'R',       :release_date => '26-Oct-1984'});
+    Movie.create({:title => 'When Harry Met Sally', :rating => 'R',  :release_date => '21-Jul-1989'});
+    Movie.create({:title => 'The Help',        :rating => 'PG-13',   :release_date => '10-Aug-2011'});
+    Movie.create({:title => 'Chocolat',        :rating => 'PG-13',   :release_date => '5-Jan-2001'});
+    Movie.create({:title => 'Amelie',          :rating => 'R',       :release_date => '25-Apr-2001'});
+    Movie.create({:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'});
+    Movie.create({:title => 'The Incredibles', :rating => 'PG',      :release_date => '5-Nov-2004'});
+    Movie.create({:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'});
+    Movie.create({:title => 'Chicken Run ',    :rating => 'G',       :release_date => '21-Jun-2000'});
   end
-  flunk "Unimplemented"
 end
-
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
